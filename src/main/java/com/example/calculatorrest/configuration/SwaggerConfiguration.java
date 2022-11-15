876c67fa9257d2d1cec196ca6908fb207170d17b
Julija15 <1771808@mail.ru>
2022-11-15 20:48:46 +0300

    add springdata 3

--- src/main/java/com/example/calculatorrest/configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/calculatorrest/configuration/SwaggerConfiguration.java
@@ -1,8 +1,5 @@
 package com.example.calculatorrest.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 //@Configuration
 public class SwaggerConfiguration {
 //    @Bean
--- src/main/java/com/example/calculatorrest/controller/UserController.java
+++ src/main/java/com/example/calculatorrest/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
+
     private final List<User> userList;
     @Autowired
     InMemoryUserStorage inMemoryUserStorage;
